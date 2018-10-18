cdafa602526e3cc83c3e83031dc3d96dfa705cc7
沈飞 <shenfei@jiangduoduo.com>
2018-10-18 18:51:18 +0800

    post-detail
    post-detail

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -2,67 +2,180 @@
 const POSTDATA = require('../../../data/posts-data.js');
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        isPlayingMusic: false
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        let _this = this;
+        let postId = options.id;
+        // this.setData({"postId":postId});
+        this.setData(POSTDATA.postList[postId]);
+        // console.log(this.data.detail);
+        // console.log(POSTDATA);
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      var postId = options.id;
+        let postsCollected = wx.getStorageSync("posts_Collected");
+        if (postsCollected) {
+            let postCollected = postsCollected[postId];
 
-  },
+            this.setData({
+                "collected": postCollected ? true : false
+            })
+        } else {
+            postsCollected = {};
+            postsCollected[postId] = false;
+            wx.setStorageSync("posts_Collected", postsCollected);
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
+        wx.onBackgroundAudioPlay(function() {
+            _this.setData({
+                "isPlayingMusic": true
+            });
+        });
+        wx.onBackgroundAudioPause(function() {
+            _this.setData({
+                "isPlayingMusic": false
+            });
+        });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
+    onCollectionTap() {
+        let postsCollected = wx.getStorageSync("posts_Collected");
+        let postCollected = postsCollected[this.data.postId] ? true : false;
 
-  },
+        postsCollected[this.data.postId] = !postCollected;
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+        // console.log(postCollected);
 
-  },
+        this.showModal(postsCollected, postsCollected[this.data.postId]);
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
-  },
+    },
+    showModal(postsCollected, postCollected) {
+        let _this = this;
+        wx.showModal({
+            title: '收藏',
+            content: !postCollected ? '是否取消收藏该文章' : "是否收藏该文章",
+            showCancel: "true",
+            cancelTexT: "取消",
+            cancelColor: "#333",
+            confirmText: "确定",
+            confirmColor: '#405f80',
+            success: function(res) {
+                if (res.confirm) {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
+                    wx.setStorageSync('posts_Collected', postsCollected);
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+                    _this.setData({
+                        "collected": postCollected
+                    });
 
-  },
+                    _this.showToast(postsCollected, postCollected);
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+                } else if (res.cancel) {
 
-  }
+                }
+            }
+        })
+    },
+    showToast(postsCollected, postCollected) {
+
+
+        wx.showToast({
+            title: postCollected ? "收藏成功" : "取消成功",
+            icon: 'success'
+        })
+    },
+    onshareTap(event) {
+        let itemList = [
+            "分享到朋友圈",
+            "分享到QQ",
+            "分享到微博"
+        ]
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: '#405f80',
+            success: function(res) {
+                wx.showModal({
+                    title: '用户' + itemList[res.tapIndex],
+                    content: '现在还无法显示分享功能',
+                })
+            }
+
+        })
+    },
+    onMusicTap(event) {
+        let isPlayingMusic = this.data.isPlayingMusic;
+        if (isPlayingMusic) {
+            wx.pauseBackgroundAudio();
+            this.setData({
+                "isPlayingMusic": false
+            });
+        } else {
+            wx.playBackgroundAudio({
+                dataUrl: this.data.music.url,
+                title: this.data.music.title,
+                coverImgUrl: this.data.music.coverImg
+            });
+            this.setData({
+                "isPlayingMusic": true
+            });
+        }
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
